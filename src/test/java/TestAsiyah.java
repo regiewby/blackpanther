ec31ab9f233a95d8ea477563f93df6d75f87d964
nurasiyahh <nurasiyah727@gmail.com>
2023-06-15 22:09:13 +0700

    Test

--- src/test/java/TestAsiyah.java
+++ src/test/java/TestAsiyah.java
@@ -19,5 +19,5 @@ public class TestAsiyah {
       webDriver.findElement(By .id("shopping_chart_container")).click();
       webDriver.findElement(By .id("checkout-button")).click();
         webDriver.close();
-
+    }
 }
