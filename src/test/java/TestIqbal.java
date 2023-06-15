5c4478bb65b9769e89f396fea5257860819acdfd
iqbalmmm <iqbal.itang@gmail.com>
2023-06-15 21:20:29 +0700

    add test iqbal

---
+++ src/test/java/TestIqbal.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestIqbal {
+    public static void main(String[] args) throws MalformedURLException {
+
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().fullscreen();
+
+        webDriver.navigate().to(new URL("https://www.saucedemo.com/"));
+
+        webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        webDriver.findElement(By.id("login-button")).click();
+
+        webDriver.findElement(By.id("shopping_cart_container")).click();
+
+        webDriver.findElement(By.id("checkout")).click();
+
+//        webDriver.close();
+    }
+}
