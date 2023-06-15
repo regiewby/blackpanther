8484a19a442b4150053d27f89a75dbf1a2ba181b
sitinajmiaihvasyani <sitinajmiaihvasyani@gmail.com>
2023-06-15 21:36:51 +0700

    Revisi file TestNajmia.java

--- src/test/java/TestNajmia.java
+++ src/test/java/TestNajmia.java
@@ -10,23 +10,15 @@ public class TestNajmia {
 
     public static void main(String[] args) throws MalformedURLException {
 
-        // used for open chrome driver
         WebDriver webDriver = new ChromeDriver();
 
-        // used for setup screen browser
         webDriver.manage().window().fullscreen();
 
-        // used for navigate to web url
         webDriver.navigate().to(new URL("https://www.saucedemo.com/"));
 
-        // insert value
         webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
 
-        // insert value
         webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
-
-        // click element
-        webDriver.findElement(By.id("login-button")).click();
 
         webDriver.findElement(By.id("login-button")).click();
 
@@ -34,7 +26,6 @@ public class TestNajmia {
 
         webDriver.findElement(By.id("checkout")).click();
 
-        // close browser
         webDriver.close();
     }
 }
