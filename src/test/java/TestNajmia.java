3dfad4110b7cd10ec6d89a480103ba74b9e94d62
sitinajmiaihvasyani <sitinajmiaihvasyani@gmail.com>
2023-06-15 21:33:43 +0700

    Add file TestNajmia.java

---
+++ src/test/java/TestNajmia.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestNajmia {
+
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        // used for open chrome driver
+        WebDriver webDriver = new ChromeDriver();
+
+        // used for setup screen browser
+        webDriver.manage().window().fullscreen();
+
+        // used for navigate to web url
+        webDriver.navigate().to(new URL("https://www.saucedemo.com/"));
+
+        // insert value
+        webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        // insert value
+        webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // click element
+        webDriver.findElement(By.id("login-button")).click();
+
+        webDriver.findElement(By.id("login-button")).click();
+
+        webDriver.findElement(By.id("shopping_cart_container")).click();
+
+        webDriver.findElement(By.id("checkout")).click();
+
+        // close browser
+        webDriver.close();
+    }
+}
