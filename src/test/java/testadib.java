a1aab8c0956f22535394d6525db7d09570fe4e56
adib1709 <hidup.baru1796@gmail.com>
2023-06-15 22:50:18 +0700

    push

--- src/test/java/testadib.java
+++ src/test/java/testadib.java
@@ -7,45 +7,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class testadib {
-}
 
     public static void main(String[] args) throws MalformedURLException {
 
-        // used for open chrome driver
-        WebDriver webDriver = new ChromeDriver();
 
-        // used for setup screen browser
+        WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().fullscreen();
 
-        // used for navigate to web url
         webDriver.navigate().to(new URL("https://www.saucedemo.com/"));
-
-        // insert value
         webDriver.findElement(By.id("user-name")).sendKeys("problem_user");
-
-        // insert value
         webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
-
-        // click element
         webDriver.findElement(By.id("login-button")).click();
+        webDriver.findElement(By .id("shopping_chart_container")).click();
+        webDriver.findElement(By .id("checkout-button")).click();
+        webDriver.close();
 
-
-            webDriver.findElement(By.ByName("Products")).sendKeys("sauslabsbakcpack");
-
-            // click element
-            webDriver.findElement()By.id("addtocart-button")) .click;
-
-
-
-
-
-
-
-
-
-        }
-
-    }
     }
 
 }
